fea08b19f306999fbe65a26537c7c00807e16934
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2021-12-11 20:49:05 +0600

    Request Run Time Storage  Permission

--- app/src/main/java/com/azhar/videoplayerusingexoplayer/Activity/MainActivity.kt
+++ app/src/main/java/com/azhar/videoplayerusingexoplayer/Activity/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.azhar.videoplayerusingexoplayer.Activity
 
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.azhar.videoplayerusingexoplayer.Fragment.FoldersFragment
 import com.azhar.videoplayerusingexoplayer.Fragment.VideosFragment
@@ -17,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.CoolTheme)
         setContentView(binding.root)
 
+        requestRunTimePermission()
         setFragment(VideosFragment())
         binding.bottomNav.setOnItemSelectedListener {
             when(it.itemId){
@@ -34,4 +38,28 @@ class MainActivity : AppCompatActivity() {
         transaction.disallowAddToBackStack()
         transaction.commit()
     }
+
+    private fun requestRunTimePermission():Boolean {
+        if (ActivityCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE), 13)
+            return false
+        }
+
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 13){
+            if (grantResults[0]==PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            else
+                ActivityCompat.requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE),  13)
+        }
+    }
+
 }
