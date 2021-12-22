ad95bdd7c8677614879a794e87925c56ff21bb4b
azharul <mdazharcse14@gmail.com>
2021-12-22 08:30:44 +0600

    Add PlayerActivity.kt

---
+++ app/src/main/java/com/azhar/videoplayerusingexoplayer/View/Activity/PlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.azhar.videoplayerusingexoplayer.View.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.azhar.videoplayerusingexoplayer.R
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
